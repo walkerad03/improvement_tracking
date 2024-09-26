8a03f1a2030299409334b970c6ad1a11da17d805
Walker Davis <wdavis@unc.edu>
2024-09-26 13:16:03 -0400

    Cleanup

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
+import argparse
+
 import polars as pl
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-PLOT_PERCENT_IMPROVEMENTS = False
+# Plot histogram of performance improvement percentages
+PLOT_PERCENT_IMPROVEMENTS = True
+
+# Parse command line args
+parser = argparse.ArgumentParser()
+parser.add_argument("--file", help="Relative filepath for times")
+
+filepath = parser.parse_args().file
+
+assert filepath is not None, "Must supply file (--file <filepath>)"
 
 # Importing data
-df = pl.scan_csv("data/davis_50fr.csv")
+df = pl.scan_csv(filepath)
+
+# Format times from MM:SS.MS to SS.MS
+times_vec = df.select(pl.col("Time")).collect().to_series().to_list()
+
+for idx, time in enumerate(times_vec):
+    time_str = str(time)
+    if ":" in time_str:
+        mins = int(time_str.split(":")[0])
+        secs = int(time_str.split(":")[1].split(".")[0])
+        ms = float(time_str.split(".")[1])
+
+        time_sec_ms = round(mins * 60 + secs + ms / 100, 2)
+    else:
+        secs = int(time_str.split(".")[0])
+        ms = int(time_str.split(".")[1])
+        time_sec_ms = round(secs + ms / 100, 2)
+
+    times_vec[idx] = time_sec_ms
+
+df = df.with_columns(pl.Series("Time", times_vec))
 
 # Formatting dates
 df = df.select(pl.col("Time", "Date")).with_columns(
@@ -15,12 +46,20 @@ df = df.select(pl.col("Time", "Date")).with_columns(
     .str.to_datetime()
 )
 
+# Plot swim times as a function of date swam
+sns.lineplot(data=df.collect(), x="Date", y="Time")
+plt.xlabel("Date")
+plt.ylabel("Time (s)")
+plt.title("Swimming results over time")
+plt.show()
+
 # Calculating current best time for each swim
 df = df.sort(["Date", "Time"])
 
 # Only keep the fastest swim per day
 df = df.unique(subset=["Date"], keep="first")
 
+# Find personal best time at time of swim
 df = df.with_columns(
     pl.col("Time")
     .cum_min()
@@ -28,10 +67,6 @@ df = df.with_columns(
     .alias("current_best")
 )
 
-sns.lineplot(data=df.collect(), x="Date", y="Time")
-plt.show()
-
-print(df.collect())
 
 # Computing time between swims
 df = df.with_columns(pl.col("Date").diff().alias("time_diff").dt.total_days())
@@ -60,9 +95,13 @@ df = df.with_columns(
 
 
 if PLOT_PERCENT_IMPROVEMENTS:
-    sns.histplot(data=df.collect(), x="scaled_percent_improvement", bins=75)
+    sns.histplot(data=df.collect(), x="scaled_percent_improvement")
+    plt.xlabel("Scaled Percent Improvements")
+    plt.ylabel("Count")
+    plt.title("Distribution of Scaled Percent Improvements")
     plt.show()
 
+# Compute Adjusted Z-Scores
 median_pct_imp = (
     df.select(pl.col("scaled_percent_improvement")).median().collect().item()
 )
@@ -82,6 +121,8 @@ df = df.with_columns(
     ).alias("modified_z_score")
 )
 
+# Display flagged times and output full details to disk
+print("Flagged Swims")
 print(
     df.select(
         [
@@ -96,3 +137,5 @@ print(
     .filter(pl.col("modified_z_score") > 3.5)
     .collect()
 )
+
+df.collect().write_csv("flagged_swims.csv")
